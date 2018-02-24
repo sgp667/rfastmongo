9b2a3051e3cd8f903c587c128ae045125c4aa06c
Tomasz Cholewik <tomasz.cholewik@gmail.com>
2018-02-23 22:23:29 -0500

    Corrected parser.

--- R/paser.R
+++ R/paser.R
@@ -1,4 +1,8 @@
+build_query <- function(x, prettify = FALSE) {
+  query <- list(x,NULL)[[1+is_empty_query(x)]]
+  jsonlite::toJSON(query,pretty = prettify,auto_unbox = TRUE,Date = "ISO8601",POSIXt = "mongo")
+}
 
-build_query <- function(x) {
-  jsonlite::toJSON(x)
+is_empty_query <- function(x) {
+  is.list(x) & length(x) == 0
 }
--- tests/testthat/test_parsing.R
+++ tests/testthat/test_parsing.R
@@ -1,8 +1,21 @@
 context("Parsting of query operators")
+library(jsonlite)
+
+test_that("Queries can be parsed into JSON",{
+  parser_output <- build_query(list(a = 1))
+  expected_json <- toJSON(list(a = 1),auto_unbox = TRUE)
+
+  expect_is(parser_output,"json")
+  expect_equal(parser_output,expected_json,
+               info = paste0("Output: ",parser_output,"\n","Expected: ",expected_json))
+
+})
 
 test_that("Empty queries are parsed as curly brackets", {
   parser_output <- build_query(list())
+  expected_json <- toJSON(NULL,auto_unbox = TRUE)
+
   expect_is(parser_output,"json")
-  expect_equal(parser_output,"{}",
-               info = paste("Output:",parser_output))
+  expect_equal(parser_output,expected_json,
+               info = paste0("Output: ",parser_output,"\n","Expected: ",expected_json))
 })
