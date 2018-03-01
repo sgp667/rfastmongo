f10c9da4d1fdeb489a0e3bb9794884363fcc5483
Tomasz Cholewik <tomasz.cholewik@markit.com>
2018-03-01 17:14:38 -0500

    Updated package version, corrected unit test

--- tests/testthat/test_query.R
+++ tests/testthat/test_query.R
@@ -2,7 +2,7 @@
 context("Query composition tools")
 test_that("Queries can be composed using pipes",{
   unfinished_query <- query(lte(1)) %>%
-    query(ne(0)) %>%
+    query(ne(0))
 
   expect_equal_tree(
     unfinished_query,
