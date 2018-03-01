9e79a17315523a1ee8154c0a7d4ca8adf4fe2d82
Tomasz Cholewik <tomasz.cholewik@markit.com>
2018-03-01 17:05:13 -0500

    Added unit test, documentation for query function.

---
+++ tests/testthat/test_query.R
@@ -0,0 +1,11 @@
+
+context("Query composition tools")
+test_that("Queries can be composed using pipes",{
+  unfinished_query <- query(lte(1)) %>%
+    query(ne(0)) %>%
+
+  expect_equal_tree(
+    unfinished_query,
+    list(`$lte` = list(1),
+         `$ne`  = list(0)))
+})
